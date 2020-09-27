dcfa373a16367e6c481a561ffe0f3ab506b23b23
Anderson lairton <lairton.anderson@gmail.com>
2020-09-27 17:58:11 -0300

    aula de js basico

---
+++ AulaIntroJs/js/script.js
@@ -0,0 +1,5 @@
+console.log('ola mundo js');
+console.log('arquivo js da pagina intro js');
+var title = document.querySelector('h1'); //alterando todos os h1 via js
+
+title.textContent = 'Alterei no js'; //alterando o texto do elemento
