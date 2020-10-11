a2ac76b580a02b85ca708f6dbe0ecc0301e37af2
Anderson lairton <lairton.anderson@gmail.com>
2020-10-11 18:41:56 -0300

    listando os 5 estados com mais cidades

--- trabalhoPraticoModulo2resolvido/index.js
+++ trabalhoPraticoModulo2resolvido/index.js
@@ -10,7 +10,8 @@ import { promises as fs } from 'fs';
 init();
 async function init() {
   await createFiles();
-  await getCitiesCount('MG');
+  // await getCitiesCount('MG');
+  await getStatesWithMoreCities();
 }
 async function createFiles() {
   let data = await fs.readFile('./cidades-estados-brasil-json/Estados.json');
@@ -35,5 +36,30 @@ async function getCitiesCount(uf) {
   const data = await fs.readFile(`./states/${uf}.json`); //lendo os arquivos da pasta state com base na uf passada como paramentro
   const cities = JSON.parse(data);
 
-  console.log(cities.length);
+  // console.log(cities.length);
+  return cities.length;
+}
+
+async function getStatesWithMoreCities() {
+  const states = JSON.parse(
+    await fs.readFile('./cidades-estados-brasil-json/Estados.json')
+  );
+
+  const list = [];
+
+  for (let state of states) {
+    //passando as uf para a função de contagem
+    const count = await getCitiesCount(state.Sigla);
+    list.push({ uf: state.Sigla, count });
+  }
+  list.sort((b, a) => {
+    //ordenando do menor para o maior
+    if (a.count > b.count) return 1;
+    else if (a.count < b.count) return -1;
+    else return 0;
+  });
+  const result = [];
+  list.slice(0, 5); //mostrando apenas 5 elementos do array
+  list.slice(0, 5).forEach((item) => result.push(item.uf + '-' + item.count));
+  console.log(result);
 }
