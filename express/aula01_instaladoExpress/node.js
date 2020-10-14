8fae4dded92da70a0dca64d108754e2dae1599ea
andersonlairton <42753094+andersonlairton@users.noreply.github.com>
2020-10-13 22:32:42 -0300

    criando rotas no nodejs

--- express/aula01_instaladoExpress/node.js
+++ express/aula01_instaladoExpress/node.js
@@ -1,7 +1,21 @@
-import { express } from 'express'; //importando express
+import express from 'express'; //importando express
 
 const app = express(); //criando uma instancia do express;
 
 app.get('/', (req, res) => {
-  res;
+  res.send('hello world');
 });
+
+
+app.post('/',(req,res)=>{
+  const a = 3;
+  const b = 5;
+  const resultado = a + b;
+
+  res.send("Resultado :" + resultado);  
+});
+
+app.listen(3000,()=>{//definindo a porta e chamando uma callback
+  console.log('api iniciada');
+});
+
